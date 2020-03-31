d24325fce22741a90fcc12ce72d3ecf5a3a0242b
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-03-31 18:29:43 +0200

    Update response key check for invoice

--- classes/gateways/class-pb2b-v1-invoice-gateway.php
+++ classes/gateways/class-pb2b-v1-invoice-gateway.php
@@ -221,7 +221,7 @@ class PB2B_V1_Invoice_Gateway extends PB2B_Factory_Gateway {
 
 			update_post_meta( $order_id, '_payer_order_id', sanitize_key( $response['orderId'] ) );
 			update_post_meta( $order_id, '_payer_reference_id', sanitize_key( $response['referenceId'] ) );
-			update_post_meta( $order_id, '_payer_customer_type', sanitize_key( $response['b2B'] ? 'B2B' : 'B2C' ) );
+			update_post_meta( $order_id, '_payer_customer_type', sanitize_key( $response['isB2B'] ? 'B2B' : 'B2C' ) );
 			$order->payment_complete( $response['orderId'] );
 			$order->add_order_note( __( 'Payment made with Payer', 'payer-b2b-for-woocommerce' ) );
 		} else {
