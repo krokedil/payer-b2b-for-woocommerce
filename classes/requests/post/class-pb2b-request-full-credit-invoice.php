63d1ad32af50a883452f9fbf52ea7add685306c8
MichaelBengtsson <michael@krokedil.se>
2020-09-10 10:18:40 +0200

    Move and update credit invoice classes

--- classes/requests/post/class-pb2b-request-full-credit-invoice.php
+++ classes/requests/post/class-pb2b-request-full-credit-invoice.php
@@ -16,24 +16,11 @@ class PB2B_Request_Credit_Invoice extends PB2B_Request {
 	/**
 	 * Makes the request.
 	 *
-	 * @param float  $amount The refund amount.
-	 * @param string $reason The reason for the refund.
 	 * @return array
 	 */
-	public function request( $amount, $reason ) {
-		error_log( 'Full refund Class' );
-
-		$payer_order_id = get_post_meta( $this->order_id, '_payer_order_id', true );
-
-		// $request_url    = $this->base_url . '/api/v1/orders/' . $payer_order_id . '/credit';
-		// Full refund
-		$request_url = $this->base_url . '/api/v2/orders/invoices/credit/full';
-
-		// error_log( 'Request URL: ' . $request_url );
-		// error_log( $payer_order_id );
-
-		// $request_args = apply_filters( 'payer_credit_v1_invoice_args', $this->get_request_args( $this->order_id, $amount, $reason ), $this->order_id );
-		$request_args = apply_filters( 'payer_credit_v2_invoice_args', $this->get_request_args( $this->order_id, $amount, $reason ), $this->order_id );
+	public function request() {
+		$request_url  = $this->base_url . '/api/v2/orders/invoices/credit/full';
+		$request_args = apply_filters( 'payer_credit_v2_invoice_args', $this->get_request_args( $this->order_id ), $this->order_id );
 		$response     = wp_remote_request( $request_url, $request_args );
 		$code         = wp_remote_retrieve_response_code( $response );
 
@@ -55,15 +42,11 @@ class PB2B_Request_Credit_Invoice extends PB2B_Request {
 	 * @param string $reason The reason for the refund.
 	 * @return array
 	 */
-	public function get_request_args( $order_id, $amount, $reason ) {
-
-		// error_log( var_export( $this->get_body( $order_id, $amount, $reason ), true ) );
-
-		// error_log( wc_get_order( $order_id ) );
+	public function get_request_args( $order_id ) {
 		return array(
 			'headers' => $this->get_headers(),
 			'method'  => 'POST',
-			'body'    => wp_json_encode( $this->get_body( $order_id, $amount, $reason ) ),
+			'body'    => wp_json_encode( $this->get_body( $order_id ) ),
 		);
 	}
 
@@ -75,8 +58,7 @@ class PB2B_Request_Credit_Invoice extends PB2B_Request {
 	 * @param string $reason The reason for the refund.
 	 * @return array
 	 */
-	public function get_body( $order_id, $amount, $reason ) {
-
+	public function get_body( $order_id ) {
 		$order = wc_get_order( $order_id );
 
 		return array(
